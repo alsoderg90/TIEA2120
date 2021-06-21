8c257f1431c86e6a306e1ee98093b878e2864f5d
alexander <alsoderg@student.jyu.fi>
2021-06-22 01:48:59 +0300

    demo 4 alku

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [HT 2 (Taso 4) ](http://users.jyu.fi/~alsoderg/tiea2120/ht2/pohja.xhtml)
 
 [HT 3 (Taso 3) ](http://users.jyu.fi/~alsoderg/tiea2120/ht3/pohja.xhtml)
+
+[HT 3 (Taso 1) ](http://users.jyu.fi/~alsoderg/tiea2120/ht4/pohja.xhtml)
--- demot/4/vt4.js
+++ demot/4/vt4.js
@@ -1,3 +1,20 @@
 "use strict";
 //@ts-check 
 
+
+window.onload = function() {
+
+	let pingu1 = document.createElement("img");
+	pingu1.src = "https://appro.mit.jyu.fi/tiea2120/vt/vt4/penguin.png";
+	pingu1.setAttribute("id", "pingu1");
+	pingu1.setAttribute("alt", "pingu1");
+	document.getElementById('body').appendChild(pingu1);
+	
+	let pingu2 = document.createElement("img");
+	pingu2.src = "https://appro.mit.jyu.fi/tiea2120/vt/vt4/penguin.png";
+	pingu2.setAttribute("id", "pingu2");
+	pingu2.setAttribute("alt", "pingu2");
+	document.getElementById('body').appendChild(pingu2);
+	
+};
+
